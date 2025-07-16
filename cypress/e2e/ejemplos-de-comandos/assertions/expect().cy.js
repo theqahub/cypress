a99e6d7fa09b8a9acd4306bb152e388d015ed308
Diego Sanchez Martin <diego.sanchez@theqahub.es>
2025-07-16 08:15:31 +0200

    + expect() spec file

---
+++ cypress/e2e/ejemplos-de-comandos/assertions/expect().cy.js
@@ -0,0 +1,17 @@
+// Funciones en Cypress 
+// Assertions: expect()
+
+describe('Tests para usar expect()', () => {
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io/commands/assertions');
+    cy.url().should('include', '/commands/assertions');
+  });
+
+  it('BDD: Uso de expect() explícito', () => {
+    expect(true).to.be.true;
+    const o = { foo: 'bar' };
+    expect(o).to.equal(o);
+    expect(o).to.deep.equal({ foo: 'bar' });
+    expect('FooBar').to.match(/bar$/i); // :contentReference[oaicite:10]{index=10}
+  });
+});
